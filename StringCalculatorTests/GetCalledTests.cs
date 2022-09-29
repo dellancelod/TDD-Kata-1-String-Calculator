64035750f9d20732791b82520750c78e58a50682
Danil <danil.svetaylo@gmail.com>
2022-09-29 15:48:38 +0300

    Add GetCalledCount method test

---
+++ StringCalculatorTests/GetCalledTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalculatorClasses;
+
+namespace StringCalculatorTests
+{
+    class GetCalledTests
+    {
+        private StringCalculator _stringCalculator { get; set; } = null;
+        [SetUp]
+        public void Setup()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Test]
+        public void GetCalledCount_countAddInvoke_3()
+        {
+            int result = _stringCalculator.Add("1,2");
+            result = _stringCalculator.Add("1,2\n3\n5,7");
+            result = _stringCalculator.Add("//;\n1;2;3;5;7");
+
+            Assert.AreEqual(3, _stringCalculator.GetCalledCount());
+        }
+
+    }
+}
