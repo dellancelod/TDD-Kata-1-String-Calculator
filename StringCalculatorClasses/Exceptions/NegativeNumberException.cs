6f332c1418ef541f360a517577964ad0c917d027
Danil <danil.svetaylo@gmail.com>
2022-09-29 15:15:41 +0300

    Add NegativeNumberException

---
+++ StringCalculatorClasses/Exceptions/NegativeNumberException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorClasses
+{
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException() { }
+        public NegativeNumberException(string message) : base(message) { }
+        public NegativeNumberException(string message, Exception inner) : base(message, inner) { }
+    }
+}
